21a2388941c250f0442402b9b9c693adc1a5c7f2
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2021-05-29 19:31:55 +0200

    Fix prototype of fn_vAskToChangeLevel in GAM

--- src/GAM/GAM.c
+++ src/GAM/GAM.c
@@ -11,4 +11,4 @@
 ACP_API BYTE (*GAM_fn_ucGetEngineMode)( void ) = 0x404C60;
 ACP_API void (*GAM_fn_vChangeEngineMode)( BYTE ucMode ) = 0x404BF0;
 ACP_API char * (*GAM_fn_p_szGetLevelName)( void ) = 0x404DA0;
-ACP_API void (*GAM_fn_vAskToChangeLevel)( char szLevelName, BYTE bSaveGame ) = 0x4054D0;
+ACP_API void (*GAM_fn_vAskToChangeLevel)( char *szLevelName, BYTE bSaveGame ) = 0x4054D0;
