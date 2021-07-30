c15f310051617723fb176cfa16d05e1a00a61878
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2021-07-30 22:23:50 +0200

    Add TMR module

---
+++ src/TMR/TMR.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <MTH.h>
+#include <apidef.h>
+
+
+///////////////////////
+//
+// TMR - Timer Module
+//
+///////////////////////
+
+
+typedef struct TMR_tdstTimerCount TMR_tdstTimerCount;
+typedef struct TMR_tdstEngineTimerStructure TMR_tdstEngineTimerStructure;
+
+
+struct TMR_tdstTimerCount
+{
+	DWORD ulLowPart;
+	DWORD ulHighPart;
+};
+
+struct TMR_tdstEngineTimerStructure
+{
+	DWORD ulFrameNumber;
+	short wTimerHandle;
+	DWORD ulCurrentTimerCount;
+	DWORD ulDeltaTimerCount;
+	DWORD a_ulCounter[16];
+	DWORD ulUsefulDeltaTime;
+	DWORD ulPauseTime;
+	float xFrameLength;
+	TMR_tdstTimerCount stRealTimeCount;
+	TMR_tdstTimerCount stPauseTime;
+	DWORD ulTickPerMs;
+};
