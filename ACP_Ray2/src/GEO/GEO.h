22b492cabf8d1a096c35836e8144920ecc2c8ce3
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2022-03-15 22:29:51 +0100

    Add geometric and PO objects

--- ACP_Ray2/src/GEO/GEO.h
+++ ACP_Ray2/src/GEO/GEO.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#include "../basedef.h"
+#include "../GMT/GMT.h"
+#include "../MTH.h"
 #include "../apidef.h"
 
 
@@ -11,14 +12,95 @@
  ****************************************************************************/
 
 
+/* Constant for no element parallel boxes */
+#define GEO_C_xNoParallelBox ((ACP_tdxIndex)-1)
+
+
 typedef float GEO_tdxColorValue;
-typedef struct GEO_tdstColor GEO_tdstColor;
+typedef void *GEO_tdxHandleToRadiosity;
+typedef struct GEO_tdstGeometricObject GEO_tdstGeometricObject;
 
-
-struct GEO_tdstColor
+typedef struct GEO_tdstColor
 {
 	GEO_tdxColorValue xR;
 	GEO_tdxColorValue xG;
 	GEO_tdxColorValue xB;
 	GEO_tdxColorValue xA;
-};
+} GEO_tdstColor;
+
+/*
+ * Bounding volume: parallel box and sphere
+ */
+
+typedef struct GEO_tdstParallelBox
+{
+	MTH3D_tdstVector stMinPoint;
+	MTH3D_tdstVector stMaxPoint;
+}
+GEO_tdstParallelBox;
+
+typedef struct GEO_tdstBoundingSphere
+{
+	MTH3D_tdstVector stCenterPoint;
+	MTH_tdxReal xRadius;
+}
+GEO_tdstBoundingSphere;
+
+typedef struct GEO_tdstDoubledIndex
+{
+	ACP_tdxIndex a2_xIndex[2];
+}
+GEO_tdstDoubledIndex;
+
+typedef struct GEO_tdstTripledIndex
+{
+	ACP_tdxIndex a3_xIndex[3];
+}
+GEO_tdstTripledIndex;
+
+
+typedef struct GEO_tdstVisualSet
+{
+	MTH_tdxReal xLastDistance;
+	ACP_tdxIndex xNbLodDefinitions;
+	MTH_tdxReal *d_xThresholdsTable;
+	GEO_tdstGeometricObject **d_p_stLodDefinitions;
+
+	GEO_tdxHandleToRadiosity *d_hRLI;
+	int lNbOfRLI;
+}
+GEO_tdstVisualSet;
+
+typedef struct GEO_tdstGeometricObject
+{
+	MTH3D_tdstVector *d_stListOfPoints;
+	MTH3D_tdstVector *d_stListOfPointsNormals;
+	GMT_tdstGameMaterial **d_p_stListOfPointsMaterial;
+
+	GEO_tdstColor *d_stListOfPointsReceivedLightIntensity;
+
+	/* Elements */
+	ACP_tdxIndex *d_xListOfElementsTypes;
+	void **d_hListOfElements;
+
+	/* COL_tdstOctree * */ void *p_stOctree;
+
+	/* Edges */
+	GEO_tdstDoubledIndex *d_stListOfEdges;
+	GMT_tdstGameMaterial **d_p_stListOfEdgesMaterial;
+
+	/* Bounding volume (for element) */
+	GEO_tdstParallelBox *d_stListOfParallelBoxes;
+
+	DWORD ulType;
+
+	ACP_tdxIndex xNbPoints;
+	ACP_tdxIndex xNbElements;
+	ACP_tdxIndex xNbEdges;
+	ACP_tdxIndex xNbParallelBoxes;
+
+	/* Object bounding sphere */
+	MTH_tdxReal xBoundingSphereRadius;
+	MTH3D_tdstVector xBoundingSphereCenter;
+}
+GEO_tdstGeometricObject;
---
+++ ACP_Ray2/src/PO/PO.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../GEO/GEO.h"
+#include "../basedef.h"
+#include "../apidef.h"
+
+
+/*****************************************************************************
+ *
+ * PO - Physical objects module
+ *
+ *****************************************************************************/
+
+
+typedef struct PO_tdstPhysicalCollSet
+{
+	GEO_tdstGeometricObject *hZdm;
+	GEO_tdstGeometricObject *hZdd;
+	GEO_tdstGeometricObject *hZde;
+	GEO_tdstGeometricObject *hZdr;
+}
+PO_tdstPhysicalCollSet;
+
+typedef struct PO_tdstPhysicalObject
+{
+	GEO_tdstVisualSet *hVisualSet;
+	PO_tdstPhysicalCollSet *hCollideSet;
+	GEO_tdstBoundingSphere *hBoundingVolume;
+}
+PO_tdstPhysicalObject;
