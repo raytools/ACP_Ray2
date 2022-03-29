5e22b5ee5c9eb06d0d24af2cda271cb75194010b
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2022-03-29 11:09:55 +0200

    Fix ERM names

--- src/ERM/ERM.c
+++ src/ERM/ERM.c
@@ -8,8 +8,16 @@
 
 
 unsigned char (*ERM_fn_ucInitErrMsg)( void ) = OFFSET(0x4025D0);
-ERM_tdstErrorInfo * (*ERM_fn_p_stFindstMyLastErrorInfo)( unsigned long ulThreadId ) = OFFSET(0x4026B0);
-void (*ERM_fn_vClearLastError)( unsigned long ulThreadId ) = OFFSET(0x402710);
-void (*ERM_fn_vUpdateLastError)( unsigned short uwMaxFatalErr, unsigned char ucModule,
-								 unsigned long ulThreadId, unsigned short uwError,
-								 long, unsigned char, unsigned char, char *szMsg ) = OFFSET(0x402810);
+ERM_tdstLastErrorInfo * (*ERM_fn_p_stFindstMyLastErrorInfo)( unsigned long ulChannelId ) = OFFSET(0x4026B0);
+void (*ERM_fn_vClearLastError)( unsigned long ulChannelId ) = OFFSET(0x402710);
+
+void (*ERM_fn_vUpdateLastError)(
+	unsigned short uwStartOfWarningsId,
+	unsigned char ucModuleId,
+	unsigned long ulChannelId,
+	unsigned short uwErrNum,
+	long lDebugData,
+	unsigned char ucOpenInfoWindow,
+	unsigned char ucStopForDebug,
+	char *szPersonalMsg
+) = OFFSET(0x402810);
--- src/ERM/ERM.h
+++ src/ERM/ERM.h
@@ -9,17 +9,13 @@
 #include "apidef.h"
 
 
-/*
- * TODO: Update with correct names from CPA source
- */
-
-typedef struct ERM_tdstErrorInfo
+typedef struct ERM_tdstLastErrorInfo
 {
-	unsigned short uwError;
-	unsigned char ucModule;
-	unsigned long ulThreadId;
+	unsigned short uwLastErr;
+	unsigned char ucLastModuleId;
+	unsigned long ulChannelId;
 }
-ERM_tdstErrorInfo;
+ERM_tdstLastErrorInfo;
 
 
 /*
@@ -27,8 +23,16 @@ ERM_tdstErrorInfo;
  */
 
 ACP_FUNC unsigned char (*ERM_fn_ucInitErrMsg)( void );
-ACP_FUNC ERM_tdstErrorInfo * (*ERM_fn_p_stFindstMyLastErrorInfo)( unsigned long ulThreadId );
+ACP_FUNC ERM_tdstLastErrorInfo * (*ERM_fn_p_stFindstMyLastErrorInfo)( unsigned long ulThreadId );
 ACP_FUNC void (*ERM_fn_vClearLastError)( unsigned long ulThreadId );
-ACP_FUNC void (*ERM_fn_vUpdateLastError)( unsigned short uwMaxFatalErr, unsigned char ucModule,
-												unsigned long ulThreadId, unsigned short uwError,
-												long, unsigned char, unsigned char, char *szMsg );
+
+ACP_FUNC void (*ERM_fn_vUpdateLastError)(
+	unsigned short uwStartOfWarningsId,
+	unsigned char ucModuleId,
+	unsigned long ulChannelId,
+	unsigned short uwErrNum,
+	long lDebugData,
+	unsigned char ucOpenInfoWindow,
+	unsigned char ucStopForDebug,
+	char *szPersonalMsg
+);
