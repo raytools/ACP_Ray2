9c5b02e4c3dbee126c39837673d373caa9a5dfab
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2023-07-10 00:41:03 +0200

    FHK convenience macros

--- src/Ray2x/FHK/FHK.h
+++ src/Ray2x/FHK/FHK.h
@@ -10,5 +10,9 @@
 #include "Ray2x/apidefx.h"
 
 
+#define FHK_M_lCreateHook( ppfnFunc, pfnHook ) FHK_fn_lCreateHook((void **)(ppfnFunc), (void *)(pfnHook))
+#define FHK_M_lDestroyHook( ppfnFunc, pfnHook ) FHK_fn_lDestroyHook((void **)(ppfnFunc), (void *)(pfnHook))
+
+
 ACPX_API long FHK_fn_lCreateHook( void **ppfnFunc, void *pfnHook );
 ACPX_API long FHK_fn_lDestroyHook( void **ppfnFunc, void *pfnHook );
