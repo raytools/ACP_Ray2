41ea1b31407e7711dbf69a0260621ec3f69e2a35
Spitfire_x86 <33195187+spitfirex86@users.noreply.github.com>
2025-07-09 19:59:31 +0200

    add missing JFFTXT functions

--- src/JFFTXT/JFFTXT.c
+++ src/JFFTXT/JFFTXT.c
@@ -13,3 +13,6 @@ void (*JFFTXT_vDrawString)( void *pContext, JFFTXT_tdstString *p_stString ) = OF
 void (*JFFTXT_vAddText)( unsigned long ulTextId, MTH3D_tdstVector *p_stPos, char *szText, float xAlpha ) = OFFSET(0x465A50);
 void (*JFFTXT_vExtendText)( unsigned long ulTextId, JFFTXT_tdeExtendParam ulParamToChange, float xNewValue ) = OFFSET(0x465AA0);
 void (*JFFTXT_vGetStringExtents)( JFFTXT_tdstString *p_stString, MTH2D_tdstVector *p_stTopLeft, MTH2D_tdstVector *p_stBottomRight ) = OFFSET(0x465E30);
+
+void (*JFFTXT_vGetSizeValues)( float xSize, long *dx, long *dy, long *cx, long *cy ) = OFFSET(0x465DD0);
+long (*JFFTXT_lGetStringLength)( unsigned char *pText, long dx ) = OFFSET(0x465D60);
--- src/JFFTXT/JFFTXT.h
+++ src/JFFTXT/JFFTXT.h
@@ -53,3 +53,6 @@ ACP_FUNC void (*JFFTXT_vDrawString)( void *pContext, JFFTXT_tdstString *p_stStri
 ACP_FUNC void (*JFFTXT_vAddText)( unsigned long ulTextId, MTH3D_tdstVector *p_stPos, char *szText, MTH_tdxReal xAlpha );
 ACP_FUNC void (*JFFTXT_vExtendText)( unsigned long ulTextId, JFFTXT_tdeExtendParam ulParamToChange, MTH_tdxReal xNewValue );
 ACP_FUNC void (*JFFTXT_vGetStringExtents)( JFFTXT_tdstString *p_stString, MTH2D_tdstVector *p_stTopLeft, MTH2D_tdstVector *p_stBottomRight );
+
+ACP_FUNC void (*JFFTXT_vGetSizeValues)( float xSize, long *dx, long *dy, long *cx, long *cy );
+ACP_FUNC long (*JFFTXT_lGetStringLength)( unsigned char *pText, long dx );
